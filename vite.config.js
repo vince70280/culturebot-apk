316338d40fb0bef5f658cdaad5cef5c78569f94b
vince70280 <59508227+vince70280@users.noreply.github.com>
2026-06-14 08:21:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
-    },
   },
 });
